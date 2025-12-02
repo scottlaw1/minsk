f5ff6ca2c32bd0be57e256ec5a7c0d4d5d38c1c3
scottlaw1 <slawrence@gmail.com>
2025-12-01 22:01:48 -0500

    Parser now handles multiplication and division along with addition and subtraction of expressions.

--- Program.cs
+++ Program.cs
@@ -24,7 +24,13 @@ namespace mc
                 PrettyPrint(syntaxTree.Root);
                 Console.ForegroundColor = color;
 
-                if (syntaxTree.Diagnostics.Any())
+                if (!syntaxTree.Diagnostics.Any())
+                {
+                    var e = new Evaluator(syntaxTree.Root);
+                    var result = e.Evaluate();
+                    Console.WriteLine(result);
+                }
+                else
                 {
                     Console.ForegroundColor = ConsoleColor.DarkRed;
                     foreach (var diagnostic in syntaxTree.Diagnostics)
@@ -140,11 +146,17 @@ namespace mc
             if (char.IsDigit(Current))
             {
                 var start = _position;
+
                 while (char.IsDigit(Current))
                     Next();
+
                 var length = _position - start;
                 var text = _text.Substring(start, length);
-                int.TryParse(text, out var value);
+                if (!int.TryParse(text, out var value))
+                {
+                    _diagnostics.Add($"The number {_text} isn't valid Int32.");
+                }
+                
                 return new SyntaxToken(SyntaxKind.NumberToken, start, text, value);
             }
 
@@ -307,7 +319,9 @@ namespace mc
             var left = ParsePrimaryExpression();
 
             while (Current.Kind == SyntaxKind.PlusToken ||
-                   Current.Kind == SyntaxKind.MinusToken)
+                   Current.Kind == SyntaxKind.MinusToken ||
+                   Current.Kind == SyntaxKind.StarToken ||
+                   Current.Kind == SyntaxKind.SlashToken)
             {
                 var operatorToken = NextToken();
                 var right = ParsePrimaryExpression(); 
@@ -321,6 +335,46 @@ namespace mc
         {
             var numberToken = Match(SyntaxKind.NumberToken);
             return new NumberExpressionSyntax(numberToken);
+        }
+    }
+
+    class Evaluator
+    {
+        private readonly ExpressionSyntax _root;
+        public Evaluator(ExpressionSyntax root)
+        {
+            _root = root;
+        }
+
+        public int Evaluate()
+        {
+            return EvaluateExpression(_root);
+        }
+
+        private int EvaluateExpression(ExpressionSyntax node)
+        {
+            if (node is NumberExpressionSyntax n)
+                return (int)n.NumberToken.Value;
+
+
+            if (node is BinaryExpressionSyntax b)
+            {
+                var left = EvaluateExpression(b.Left);
+                var right = EvaluateExpression(b.Right);
+
+                if (b.OperatorToken.Kind == SyntaxKind.PlusToken)
+                    return left + right;
+                else if (b.OperatorToken.Kind == SyntaxKind.MinusToken)
+                    return left - right;
+                else if (b.OperatorToken.Kind == SyntaxKind.StarToken)
+                    return left * right;
+                else if (b.OperatorToken.Kind == SyntaxKind.SlashToken)
+                    return left / right;
+                else
+                    throw new Exception($"Unexpected binary operator {b.OperatorToken.Kind}");
+            }
+
+            throw new Exception($"Unexpected node {node.Kind}");
         }
     }
 }

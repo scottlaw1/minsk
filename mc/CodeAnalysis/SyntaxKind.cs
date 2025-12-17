2d8c92d482bb4255edde73e0f6bbd3c1de356fb5
scottlaw1 <slawrence@gmail.com>
2025-12-16 21:45:42 -0500

    Refactored classes out of Program.cs into their own files.

---
+++ mc/CodeAnalysis/SyntaxKind.cs
@@ -0,0 +1,19 @@
+namespace Minsk.CodeAnalysis
+{
+    internal enum SyntaxKind
+    {
+        NumberToken,
+        PlusToken,
+        StarToken,
+        BadToken,
+        WhitespaceToken,
+        EndOfFileToken,
+        MinusToken,
+        SlashToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
+        BinaryExpression,
+        NumberExpression,
+        ParenthesizedExpression
+    }
+}
